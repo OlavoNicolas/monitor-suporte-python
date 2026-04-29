0b4f97dd6f79b01641c468f636db19f0a897c0d4
Olavo N <olavo.nicolas5@gmail.com>
2026-04-29 17:30:03 -0300

    Update Esperar_Esq.py

--- Projeto/Utils/Esperar_Esq.py
+++ Projeto/Utils/Esperar_Esq.py
@@ -1,15 +1,33 @@
-from Utils.Limpar import limpar_tela
-import msvcrt
 import time
+from processo.sistema import SO
+from Utils.Limpar import limpar_tela
+
+sistema = SO()
+if sistema.testeSO() == "Windows":
+    try:
+        import msvcrt
+    except ImportError:
+        msvcrt = None
+else:
+    msvcrt = None
+
 
 def esperar_esc():
-        print("Pressione ESC para voltar...")
+    print("Pressione ESC para voltar...")
+
+    if msvcrt:
         while True:
-         if msvcrt.kbhit():  
-            tecla = msvcrt.getch()
-            
-            if tecla == b'\x1b':  
-                print("Voltando...")
-                time.sleep(2.0)
-                limpar_tela()
-                break
+            if msvcrt.kbhit():
+                tecla = msvcrt.getch()
+
+                if tecla == b'\x1b':
+                    print("Voltando...")
+                    time.sleep(2.0)
+                    limpar_tela()
+                    break
+
+    else:
+        input("Pressione Enter para voltar...")
+        print("Voltando...")
+        time.sleep(2.0)
+        limpar_tela()
